8221152e9c2de928a1e2f376ca6a12dd5a35500d
srsujon37 <srsujon97@gmail.com>
2025-07-19 01:15:03 +0600

    deploy

--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,23 @@
 import { NextResponse } from 'next/server'
+
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-  return NextResponse.redirect(new URL('/', request.url))
+
+  const token = request.cookies.get("nextjs-token")
+  console.log(token);
+
+  const dummyUserData = {
+    role : "user",
+    email : "test@admin"
+  }
+  let isServicePage = request.nextUrl.pathname.startsWith("/services");
+  let isAdmin = dummyUserData.role == 'admin'
+
+  if (isServicePage && !isAdmin) {
+    return  NextResponse.redirect(new URL('/login', request.url))
+  }
+  return NextResponse.next()
 }
  
 // See "Matching Paths" below to learn more
